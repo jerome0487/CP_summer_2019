c15e38c8dd086af650a8ee9080ec6452309ebc3f
jerome0487 <limjerome@outlook.com>
2019-03-21 10:15:36 +0100

    First Hello program

---
+++ src/src/Verifyemail.java
@@ -0,0 +1,17 @@
+public class Verifyemail {
+
+    public static void main(String[] args) {
+        String email = "example@gmail.com";
+        int x = email.length();
+        int a = email.indexOf("@");
+        boolean subString1 = (a > 1);
+        int b = email.indexOf(".") - (a + 1);
+        boolean subString2 = (b > 1);
+        int c = x - email.lastIndexOf(".");
+        boolean subString3 = (c > 1);
+        if (subString1 && subString2 && subString3) {
+            System.out.println(email);
+        } else System.out.println("Invalid Email");
+
+    }
+}
