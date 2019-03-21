a774918a3e05da58b4707cd83cbcb7f8f0dc1af5
jerome0487 <limjerome@outlook.com>
2019-03-21 10:23:15 +0100

    First Hello program

--- src/src/Verifyemail.java
+++ src/src/Verifyemail.java
@@ -2,11 +2,11 @@ public class Verifyemail {
 
     public static void main(String[] args) {
         String email = "example@gmail.com";
-        int x = email.length();
         int a = email.indexOf("@");
         boolean subString1 = (a > 1);
         int b = email.indexOf(".") - (a + 1);
         boolean subString2 = (b > 1);
+        int x = email.length();
         int c = x - email.lastIndexOf(".");
         boolean subString3 = (c > 1);
         if (subString1 && subString2 && subString3) {
