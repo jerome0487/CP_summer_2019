22ac544a901968d9836062996420f2eb2c5c1c73
jerome0487 <limjerome@outlook.com>
2019-03-21 10:15:06 +0100

    First Hello program

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,15 @@
 public class Hello {
     public static void main(String[] inputargs){
         System.out.println("Hello!!!");
+
+        int a = 5;
+        int b, c;
+        b=a;
+        c = b + 4;
+        c= new Integer(5);
+
+        System.out.println("b=" + b);
+        System.out.println("c=" + c);
+
     }
 }
---
+++ src/src/Numbers.java
@@ -0,0 +1,15 @@
+public class Numbers {
+
+    public static void main(String[] args) {
+        float f1 = 455.3f;
+        double d1 = f1;
+        //float f2 = d1;
+        int a = 6;
+        long b = a;
+        int c = Long.valueOf(b).intValue();
+        long bb = Long.valueOf(a);
+
+
+
+    }
+}
